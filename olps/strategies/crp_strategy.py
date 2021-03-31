15c992c1fc800cbef599995d7d9a79f659aad421
TayisiyaChernenko <tcx190026@utdallas.edu>
2021-03-30 22:13:10 -0500

    CRP strat and tests

---
+++ olps/strategies/crp_strategy.py
@@ -0,0 +1,7 @@
+from .strategy import Strategy
+from ..datasources.datasource import DataSource
+
+class CRPStrategy(Strategy):
+    def update_weights(self, market_data: DataSource) -> None:
+        # the weights are held in constant distribution
+        pass
