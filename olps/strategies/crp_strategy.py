4e710d6f4fa5c1c39a8ef8e302376d22d92a5d45
TayisiyaChernenko <tcx190026@utdallas.edu>
2021-04-04 21:29:31 -0500

    Fixed typo

--- olps/strategies/crp_strategy.py
+++ olps/strategies/crp_strategy.py
@@ -4,6 +4,6 @@ from ..datasources.datasource import DataSource
 class CRPStrategy(Strategy):
     def update_weights(self, market_data: DataSource) -> None:
         '''
-         the weights are hepipld in constant distribution
+         the weights are held in constant distribution
         '''
         pass
