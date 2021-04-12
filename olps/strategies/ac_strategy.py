e886eaaf2304a1f9382969200a5da6ea9c87b6fa
Travis A. Dula <travisadula@gmail.com>
2021-04-12 10:08:59 -0500

    added AC algo and DS

--- olps/strategies/ac_strategy.py
+++ olps/strategies/ac_strategy.py
@@ -1,16 +1,22 @@
 from .strategy import Strategy
-from ..datasources.datasource import DataSource
+from ..datasources.ac_datasource import ACDataSource
 import numpy as np
 
-class UPStrategy(Strategy):
-    def update_weights(self, market_data: DataSource) -> None:
-        if len(market_data.price_relatives.shape) == 1:
-            prv = market_data.price_relatives
-        else:
-            prv = market_data.price_relatives[:, -1]
-        w = ?
-        y1 = log(x[t-2w+1:t-w])
-        y2 = log(x[t-w+1:t])
-        Mcov(i, j) = (1.0/(w - 1))(y[1:i] -y1mean)T(y2,j - y2mean)
-        # might not be needed
-        #self.weights = self.weights / sum(self.weights)
+class ACStrategy(Strategy):
+    def update_weights(self, market_data: ACDataSource) -> None:
+        log_prv = market_data.log_price_relatives
+        last_log_prv = market_data.last_log_price_relatives
+        # possibly change later
+        w = 2
+        print(log_prv)
+        print(last_log_prv)
+        if log_prv is not None and last_log_prv is not None:
+            print('old:', last_log_prv)
+            print('new:', log_prv)
+            cc = np.correlate(last_log_prv, log_prv, 'same')
+            #print('cc:', cc)
+            print('sum:', sum(cc))
+            self.weights = self.weights + cc
+        #print('weights:', self.weights)
+        self.weights = self.weights / sum(self.weights)
+        print('fixed weights:', self.weights)
