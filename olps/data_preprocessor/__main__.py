9a0de283df6ab9323c3419d79230fc96cddbc88e
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2021-04-13 23:30:24 -0700

    Restructure data_preprocessor module to make use of __init__ and __main__

---
+++ olps/data_preprocessor/__main__.py
@@ -0,0 +1,35 @@
+import sys
+from pathlib import Path
+from .pdm import PortfolioDataMerger
+
+if __name__ == "__main__":
+    # Runs the CLI version to generate merged CSVs
+    print('Portfolio Data Merging Utility')
+    if len(sys.argv) <= 1:
+        print('You must pass in a folder name of stock CSVs.', file=sys.stderr)
+        sys.exit(1)
+    folder = Path(sys.argv[1])
+    if not folder.is_dir():
+        print('You must pass in a folder, not a file.', file=sys.stderr)
+        sys.exit(1)
+    # Get all files and stock names
+    files = [x for x in folder.iterdir() if not x.is_dir()
+             and not x.name.startswith('.')]
+    names = [str(x).replace('data/03-29-2021/', '').split('.')[0]
+             for x in files]
+    # Merge portfolios
+    merged = PortfolioDataMerger.merge_portfolio(files, names)
+    # Find the merged folder and create it if it doesn't exist
+    merged_folder = folder.joinpath('merged')
+    if merged_folder.exists() and not merged_folder.is_dir():
+        print(
+            f'The merged folder path {merged_folder} already exists and is a file?')
+    elif not merged_folder.exists():
+        print('Creating folder to store merged CSV...')
+        merged_folder.mkdir()
+    # Save the CSV in the merged folder
+    merged_filename = merged_folder.joinpath(f'{"-".join(sorted(names))}.csv')
+    print(f'Saving merged CSV at {merged_filename}...')
+    merged.to_csv(merged_filename)
+    # Print out stats on memory usage and data points
+    print(merged.info())
