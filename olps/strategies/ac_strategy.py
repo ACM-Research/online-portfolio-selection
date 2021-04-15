bf06d6b0de55951524cf0ec03f6e967500b79704
Travis A. Dula <travisadula@gmail.com>
2021-04-14 21:32:40 -0500

    removed debugging

--- olps/strategies/ac_strategy.py
+++ olps/strategies/ac_strategy.py
@@ -7,16 +7,7 @@ class ACStrategy(Strategy):
         log_prv = market_data.log_price_relatives
         last_log_prv = market_data.last_log_price_relatives
         # possibly change later
-        w = 2
-        print(log_prv)
-        print(last_log_prv)
         if log_prv is not None and last_log_prv is not None:
-            print('old:', last_log_prv)
-            print('new:', log_prv)
             cc = np.correlate(last_log_prv, log_prv, 'same')
-            #print('cc:', cc)
-            print('sum:', sum(cc))
             self.weights = self.weights + cc
-        #print('weights:', self.weights)
         self.weights = self.weights / sum(self.weights)
-        print('fixed weights:', self.weights)
