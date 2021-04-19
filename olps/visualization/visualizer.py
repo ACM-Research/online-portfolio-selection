2f9de00f5a32076b580b2d3baa45d28d24f35b63
Travis A. Dula <travisadula@gmail.com>
2021-04-19 00:40:42 -0500

    docstrings for visualizer

--- olps/visualization/visualizer.py
+++ olps/visualization/visualizer.py
@@ -3,14 +3,23 @@ from olps.strategies.strategy import Strategy
 from typing import *
 
 class Visualizer:
+    """
+    Class for visualizing cumulative wealth of multiple strategies
+    """
     outputfile: str
     strategies: list[Strategy]
 
     def __init__(self, outputfile: str, strategies: list[Strategy]):
+        """
+        Initialize the visualizer with already run strategies and output file
+        """
         self.outputfile = outputfile
         self.strategies = strategies
 
     def visualize(self):
+        """
+        Plots the strategies to the output file in a single graph
+        """
         print(f'Attempting to write to {self.outputfile}')
         for strategy in self.strategies:
             plt.plot(strategy.cumulative_wealth, label=f'{strategy.__class__}')
