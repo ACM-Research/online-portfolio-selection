9c2631eba3c0f5ad070efb05ce4e3c9453bb5d5b
Travis A. Dula <travisadula@gmail.com>
2021-04-23 14:20:32 -0500

    changed visualizer.py to abstract static class

--- olps/visualization/visualizer.py
+++ olps/visualization/visualizer.py
@@ -1,30 +1,15 @@
 import matplotlib.pyplot as plt
 from olps.strategies.strategy import Strategy
-from typing import *
+from abc import ABC, abstractmethod
 
-class Visualizer:
+class Visualizer(ABC):
     """
-    Class for visualizing cumulative wealth of multiple strategies
+    Abstract class for visualizing portfolios over time
     """
-    outputfile: str
-    strategies: list[Strategy]
-
-    def __init__(self, outputfile: str, strategies: list[Strategy]):
-        """
-        Initialize the visualizer with already run strategies and output file
-        """
-        self.outputfile = outputfile
-        self.strategies = strategies
-
-    def visualize(self):
+    @staticmethod
+    @abstractmethod
+    def visualize(outputfile: str, strategies: list[Strategy]):
         """
         Plots the strategies to the output file in a single graph
         """
-        print(f'Attempting to write to {self.outputfile}')
-        for strategy in self.strategies:
-            plt.plot(strategy.cumulative_wealth, label=f'{strategy.__class__}')
-        #plt.legend()
-        plt.ylabel('Cumulative return')
-        plt.xlabel('Ticks')
-        plt.title('Cumulative return of different algorithms')
-        plt.savefig(self.outputfile)
+        pass
