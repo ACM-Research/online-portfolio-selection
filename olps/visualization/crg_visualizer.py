f3a8897fe87fa62c3df09773e3f3d302c137de31
Travis A. Dula <travisadula@gmail.com>
2021-04-26 17:26:55 -0500

    improved crg labels

--- olps/visualization/crg_visualizer.py
+++ olps/visualization/crg_visualizer.py
@@ -12,8 +12,8 @@ class CRGVisualizer(Visualizer):
         Plots the strategies' cumulative return to the output file in a single graph
         """
         for strategy in strategies:
-            plt.plot(strategy.cumulative_wealth, label=f'{strategy.__class__}')
-        #plt.legend()
+            plt.plot(strategy.cumulative_wealth, label=f'{strategy.__class__}'[:-2].split('.')[-1])
+        plt.legend()
         plt.ylabel('Cumulative return')
         plt.xlabel('Ticks')
         plt.title('Cumulative return of different algorithms')
