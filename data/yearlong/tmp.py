a8fe48acc5ead07780ef4db9afbbcd96b357c750
Travis A. Dula <travisadula@gmail.com>
2021-04-28 01:23:04 -0500

    added yearlong tests and data

---
+++ data/yearlong/tmp.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+import csv
+data = []
+with open('./merged/ABBV_AMC_BGS_GME_NHI_WMT_XOM.csv') as csvf:
+    r = csv.reader(csvf)
+    for i,row in enumerate(r):
+        if not i:
+            data.append(['time', 'ask', 'bid', 'ticker'])
+            continue
+        data.append([row[0], row[1], row[4], row[-1]])
+        data[-1][0] = int(datetime.strptime(data[-1][0], '%Y-%m-%d').timestamp())
+print(data)
+
+with open('./merged/ABBV-AMC-BGS-GME-NHI-WMT-XOM.csv', 'w') as outcsv:
+    w = csv.writer(outcsv)
+    for row in data:
+        w.writerow(row)
